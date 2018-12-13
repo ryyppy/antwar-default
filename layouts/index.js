af61f99e0a5de7942e27716d15eeccf8c2a777f1
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-12-13 18:51:06 +0100

    Upgrade genType, fix layout file import

--- layouts/index.js
+++ layouts/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 
-import Layout from "./layout.re.js";
+import Layout from "./layout.gen.js";
 
 const SiteBody = ({ children, location, page: { file } }, ctx) => {
   /*
